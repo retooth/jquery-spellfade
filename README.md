383823918e7dfc46632e11c5db0f20d6096ea7ad
Fabian Vallon <fabian.vallon@gmx.de>
2014-04-17 18:56:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# SpellFade
+
+This jquery plugin makes it possible to fade in text letter by letter.
+Usage:
+```
+$("#myelement").spellFadeIn(800,100);
+$("#myelement").spellFadeOut(800,100);
+```
+The first parameter defines the duration until the whole text is displayed, while the second one defines the duration until a single letter is shown.
---
+++ jquery-spellfade.js
@@ -0,0 +1,56 @@
+$.fn.spellFadeIn = function (duration, letterduration){
+
+  var settings = $.extend({
+      duration: 800,
+      letterduration: 200,
+  }, {duration: duration, letterduration: letterduration} );
+
+  return this.each(function(){
+    if ($(this).children("[id*='__spellfade']").length > 0){ return } // block, if spellfade is still in motion
+    var html = $(this).html();
+    console.log("In");
+    console.log(html);
+    var duration_per_letter = settings.duration / html.length;
+    $(this).html("");
+    $(this).fadeIn(0);
+    for (var i=0; i<html.length;i++){
+        var newletter = $("<span id=\"__spellfade" + i + "__\"style=\"display:none\">" + html.charAt(i) + "</span>");
+        newletter.appendTo(this);
+        var letterdelay = i * duration_per_letter;
+        $("#__spellfade" + i + "__").delay(letterdelay).fadeIn(settings.letterduration);
+    }
+    var element = $(this);
+    setTimeout( function () {element.html(html);}, settings.duration);
+  });
+
+};
+
+
+$.fn.spellFadeOut = function (duration, letterduration){
+
+  var settings = $.extend({
+      duration: 800,
+      letterduration: 200,
+  }, {duration: duration, letterduration: letterduration} );
+
+  return this.each(function(){
+    if ($(this).children("[id*='__spellfade']").length > 0){ return } // block, if spellfade is still in motion
+    var html = $(this).html();
+    console.log("OUT");
+    console.log(html);
+    var newhtml = ""
+    for (var i=0; i<html.length;i++){
+        newhtml += "<span id=\"__spellfade" + i + "__\">" + html.charAt(i) + "</span>";
+    }
+    $(this).html(newhtml);
+    var duration_per_letter = settings.duration / html.length;
+    for (var i=0; i<html.length; i++){
+        var letterdelay = settings.duration - i * duration_per_letter;
+        $("#__spellfade" + i + "__").delay(letterdelay).fadeOut(settings.letterduration);
+    }
+    console.log("breakpoint");
+    var element = $(this);
+    setTimeout( function () { element.css("display", "none"); element.html(html);}, settings.duration);
+  });
+
+};
